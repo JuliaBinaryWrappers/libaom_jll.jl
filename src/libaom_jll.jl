8dd17c61de2dadd2f93172430d5381635db05277
jlbuild <juliabuildbot@gmail.com>
2026-04-15 14:25:57 +0000

    libaom_jll build 3.13.3+0

--- src/libaom_jll.jl
+++ src/libaom_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("libaom")
-JLLWrappers.@generate_main_file("libaom", UUID("a4ae2306-e953-59d6-aa16-d00cac43593b"))
+JLLWrappers.@generate_main_file("libaom", Base.UUID("a4ae2306-e953-59d6-aa16-d00cac43593b"))
 end  # module libaom_jll
