146be650c68a3391df5f3f6bd17563d9bb74a69d
jlbuild <juliabuildbot@gmail.com>
2026-08-12 14:47:22 +0000

    libaom_jll build 3.14.1+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -9,7 +9,7 @@ function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libaom,
-        "lib/libaom.3.13.3.dylib",
+        "lib/libaom.3.14.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
